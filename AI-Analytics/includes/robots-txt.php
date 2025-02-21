a572dd27b8ef0318f2d547e9cfe669e61ce82e31
smalker001 <127775904+smalker001@users.noreply.github.com>
2025-02-21 17:50:29 +0100

    Update robots-txt.php

--- AI-Analytics/includes/robots-txt.php
+++ AI-Analytics/includes/robots-txt.php
@@ -15,6 +15,10 @@ function smalk_echo_robots_txt_block($robots_txt) {
 # START SMALK AI ANALYTICS BLOCK
 # ---------------------------\n";
 
+    echo "\nAuthor: Smalk AI
+Website: https://www.smalk.me
+Description: Smalk AI delivers an enterprise-grade Content Intelligence Platform that enables marketing teams to measure, analyze, and optimize their brand and products visibility across generative AI search engines like ChatGPT, Google SGE, Microsoft Copilot, Perplexity AI, Claude, etc...\n";
+
     echo esc_textarea($robots_txt);
 
     echo "\n# ---------------------------
