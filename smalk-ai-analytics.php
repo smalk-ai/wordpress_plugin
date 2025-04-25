ea07f268ddee3257fe9f6f4ef5c38a8d1a3dbb46
smalker001 <127775904+smalker001@users.noreply.github.com>
2025-04-26 00:51:27 +0200

    Update smalk-ai-analytics.php

--- smalk-ai-analytics.php
+++ smalk-ai-analytics.php
@@ -3,13 +3,13 @@
 /*
 Plugin Name: Smalk AI Analytics
 Description: Get real-time analytics on AI agents and human visitors from AI Search, and control your brand visibility on Answer Engines (ChatGPT, Perplexity, etc.).
-Version: 1.0.5
+Version: 1.0.6
 Author URI: https://www.smalk.ai
 Author: Smalk AI
 Requires at least: 5.0
 Tested up to: 6.7
 Requires PHP: 7.0
-Stable tag: 1.0.5
+Stable tag: 1.0.6
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.html
 */
